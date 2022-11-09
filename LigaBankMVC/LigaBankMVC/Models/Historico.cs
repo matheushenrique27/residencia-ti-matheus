9a62b9f7876bea3200a4018bf48cdfedf883efb9
matheushenrique27 <m.mhsouza@gmail.com>
2022-11-09 18:29:34 -0300

    :up: atualização do sistema LigaBankMVC

---
+++ LigaBankMVC/LigaBankMVC/Models/Historico.cs
@@ -0,0 +1,8 @@
+﻿namespace LigaBankMVC.Models
+{
+    public class Historico : Entidade
+    {
+        public List<Operacao>? Operacoes { get; set; }
+        public Conta? Conta { get; set; }
+    }
+}
